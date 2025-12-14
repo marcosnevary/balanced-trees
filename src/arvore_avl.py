d23fc14f8cfecd3b678db0179d789269d3867f23
marcos.nevary@gmail.com <marcos.nevary@gmail.com>
2025-12-14 10:51:02 -0300

    feat: add arvore_avl.py and arvore_rubro_negra.py

---
+++ src/arvore_avl.py
@@ -0,0 +1,181 @@
+# Marcos Vinicius Brito de Araujo - 202404940009
+# Maurício Aires Pinheiro - 202404940003
+
+class NodeAVL:
+    def __init__(self, chave):
+        self.chave = chave
+        self.esquerda = None
+        self.direita = None
+        self.altura = 1
+
+class ArvoreAVL:
+    def __init__(self):
+        self.raiz = None
+
+    def _altura(self, node):
+        return node.altura if node else 0
+
+    def _obter_balanco(self, node):
+        return self._altura(node.esquerda) - self._altura(node.direita) if node else 0
+
+    def _rotacionar_direita(self, node_desbalanceado):
+        pivo = node_desbalanceado.esquerda
+        auxiliar = pivo.direita
+
+        pivo.direita = node_desbalanceado
+        node_desbalanceado.esquerda = auxiliar
+
+        node_desbalanceado.altura = 1 + max(self._altura(node_desbalanceado.esquerda), self._altura(node_desbalanceado.direita))
+        pivo.altura = 1 + max(self._altura(pivo.esquerda), self._altura(pivo.direita))
+
+        return pivo
+
+    def _rotacionar_esquerda(self, node_desbalanceado):
+        pivo = node_desbalanceado.direita
+        auxiliar = pivo.esquerda
+
+        pivo.esquerda = node_desbalanceado
+        node_desbalanceado.direita = auxiliar
+
+        node_desbalanceado.altura = 1 + max(self._altura(node_desbalanceado.esquerda), self._altura(node_desbalanceado.direita))
+        pivo.altura = 1 + max(self._altura(pivo.esquerda), self._altura(pivo.direita))
+
+        return pivo
+
+    def inserir(self, chave):
+        self.raiz = self._inserir_recursivo(self.raiz, chave)
+
+    def _inserir_recursivo(self, node_atual, chave):
+        if not node_atual:
+            return NodeAVL(chave)
+        
+        if chave < node_atual.chave:
+            node_atual.esquerda = self._inserir_recursivo(node_atual.esquerda, chave)
+
+        elif chave > node_atual.chave:
+            node_atual.direita = self._inserir_recursivo(node_atual.direita, chave)
+
+        else:
+            return node_atual # Chaves duplicadas não são inseridas
+
+        node_atual.altura = 1 + max(self._altura(node_atual.esquerda), self._altura(node_atual.direita))
+        balanco = self._obter_balanco(node_atual)
+
+        # Balanceamento
+        if balanco > 1 and chave < node_atual.esquerda.chave: # Pra esquerda
+            return self._rotacionar_direita(node_atual)
+
+        if balanco < -1 and chave > node_atual.direita.chave: # Pra direita
+            return self._rotacionar_esquerda(node_atual)
+
+        if balanco > 1 and chave > node_atual.esquerda.chave: # Dupla pra direita
+            node_atual.esquerda = self._rotacionar_esquerda(node_atual.esquerda)
+            return self._rotacionar_direita(node_atual)
+
+        if balanco < -1 and chave < node_atual.direita.chave: # Dupla pra esquerda
+            node_atual.direita = self._rotacionar_direita(node_atual.direita)
+            return self._rotacionar_esquerda(node_atual)
+
+        return node_atual
+
+    def remover(self, chave):
+        self.raiz = self._remover_recursivo(self.raiz, chave)
+
+    def _remover_recursivo(self, node_atual, chave):
+        if not node_atual:
+            return node_atual
+
+        if chave < node_atual.chave:
+            node_atual.esquerda = self._remover_recursivo(node_atual.esquerda, chave)
+
+        elif chave > node_atual.chave:
+            node_atual.direita = self._remover_recursivo(node_atual.direita, chave)
+
+        else:
+            if not node_atual.esquerda:
+                return node_atual.direita
+            
+            elif not node_atual.direita:
+                return node_atual.esquerda
+            
+            temp = self._obter_minimo(node_atual.direita)
+            node_atual.chave = temp.chave
+            node_atual.direita = self._remover_recursivo(node_atual.direita, temp.chave)
+
+        if not node_atual:
+            return node_atual
+
+        node_atual.altura = 1 + max(self._altura(node_atual.esquerda), self._altura(node_atual.direita))
+        balanco = self._obter_balanco(node_atual)
+
+        # Balanceamento
+        if balanco > 1 and self._obter_balanco(node_atual.esquerda) >= 0: # Pra direita
+            return self._rotacionar_direita(node_atual)
+        
+        if balanco > 1 and self._obter_balanco(node_atual.esquerda) < 0: # Dupla pra direita
+            node_atual.esquerda = self._rotacionar_esquerda(node_atual.esquerda)
+            return self._rotacionar_direita(node_atual)
+        
+        if balanco < -1 and self._obter_balanco(node_atual.direita) <= 0: # Pra esquerda
+            return self._rotacionar_esquerda(node_atual)
+        
+        if balanco < -1 and self._obter_balanco(node_atual.direita) > 0: # Dupla pra esquerda
+            node_atual.direita = self._rotacionar_direita(node_atual.direita)
+            return self._rotacionar_esquerda(node_atual)
+
+        return node_atual
+
+    def _obter_minimo(self, node):
+        atual = node
+        while atual.esquerda:
+            atual = atual.esquerda
+        return atual
+
+    def pesquisar(self, chave):
+        self._pesquisar_recursivo(self.raiz, chave)
+
+    def _pesquisar_recursivo(self, node, chave):
+        if not node:
+            print(f'Não achou a chave {chave}')
+            return None
+        
+        elif node.chave == chave:
+            print(f'Achou a chave {chave}')
+            return node
+
+        if chave < node.chave:
+            self._pesquisar_recursivo(node.esquerda, chave)
+
+        else:
+            self._pesquisar_recursivo(node.direita, chave)
+
+    def mostrar(self):
+        if not self.raiz:
+            print("Árvore Vazia")
+            return
+
+        self._imprimir_arvore(self.raiz, "", True)
+
+    def _imprimir_arvore(self, node, prefixo="", cauda=True):
+        if node.direita:
+            prefixo_novo = prefixo + ("│   " if cauda else "    ")
+            self._imprimir_arvore(node.direita, prefixo_novo, False)
+        
+        print(prefixo + ("└── " if cauda else "┌── ") + str(node.chave))
+        
+        if node.esquerda:
+            prefixo_novo = prefixo + ("    " if cauda else "│   ")
+            self._imprimir_arvore(node.esquerda, prefixo_novo, True)
+
+
+arvore_avl = ArvoreAVL()
+valores = [50, 25, 75, 15, 35, 60, 120, 10, 68, 90, 125, 83, 100, 25, 3, 7, 12, 84, 302, 1, 123, 101]
+
+for valor in valores:
+    arvore_avl.inserir(valor)
+
+chave = 83
+print(f"Pesquisando chave {chave}:")
+arvore_avl.pesquisar(chave)
+
+arvore_avl.mostrar()
---
+++ src/arvore_rubro_negra.py
@@ -0,0 +1,184 @@
+# Marcos Vinicius Brito de Araujo - 202404940009
+# Maurício Aires Pinheiro - 202404940003
+
+class NodeRubroNegra:
+    def __init__(self, chave):
+        self.chave = chave
+        self.esquerda = None
+        self.direita = None
+        self.pai = None
+        self.cor = 1 # 1 = Vermelho, 0 = Preto
+
+class ArvoreRubroNegra:
+    def __init__(self):
+        self.NIL = NodeRubroNegra(None)
+        self.NIL.cor = 0
+        self.raiz = self.NIL
+
+    def _rotacionar_esquerda(self, node_desbalanceado):
+        pivo = node_desbalanceado.direita
+        node_desbalanceado.direita = pivo.esquerda
+        if pivo.esquerda != self.NIL:
+            pivo.esquerda.pai = node_desbalanceado
+
+        pivo.pai = node_desbalanceado.pai
+        if node_desbalanceado.pai == None:
+            self.raiz = pivo
+
+        elif node_desbalanceado == node_desbalanceado.pai.esquerda:
+            node_desbalanceado.pai.esquerda = pivo
+
+        else:
+            node_desbalanceado.pai.direita = pivo
+
+        pivo.esquerda = node_desbalanceado
+        node_desbalanceado.pai = pivo
+
+    def _rotacionar_direita(self, node_desbalanceado):
+        pivo = node_desbalanceado.esquerda
+        node_desbalanceado.esquerda = pivo.direita
+        if pivo.direita != self.NIL:
+            pivo.direita.pai = node_desbalanceado
+
+        pivo.pai = node_desbalanceado.pai
+        if node_desbalanceado.pai == None:
+            self.raiz = pivo
+
+        elif node_desbalanceado == node_desbalanceado.pai.direita:
+            node_desbalanceado.pai.direita = pivo
+
+        else:
+            node_desbalanceado.pai.esquerda = pivo
+
+        pivo.direita = node_desbalanceado
+        node_desbalanceado.pai = pivo
+
+    def inserir(self, chave):
+        novo_node = NodeRubroNegra(chave)
+        novo_node.esquerda = self.NIL
+        novo_node.direita = self.NIL
+        
+        pai_aux = None
+        atual = self.raiz
+
+        while atual != self.NIL:
+            pai_aux = atual
+            if novo_node.chave < atual.chave:
+                atual = atual.esquerda
+
+            else:
+                atual = atual.direita
+
+        novo_node.pai = pai_aux
+        if pai_aux == None:
+            self.raiz = novo_node
+
+        elif novo_node.chave < pai_aux.chave:
+            pai_aux.esquerda = novo_node
+
+        else:
+            pai_aux.direita = novo_node
+
+        if novo_node.pai == None:
+            novo_node.cor = 0
+            return
+
+        if novo_node.pai.pai == None:
+            return
+
+        self._ajustar_insercao(novo_node)
+
+    def _ajustar_insercao(self, node_inserido):
+        while node_inserido.pai.cor == 1:
+            if node_inserido.pai == node_inserido.pai.pai.direita:
+                tio = node_inserido.pai.pai.esquerda
+                if tio.cor == 1:
+                    tio.cor = 0
+                    node_inserido.pai.cor = 0
+                    node_inserido.pai.pai.cor = 1
+                    node_inserido = node_inserido.pai.pai
+
+                else:
+                    if node_inserido == node_inserido.pai.esquerda:
+                        node_inserido = node_inserido.pai
+                        self._rotacionar_direita(node_inserido)
+
+                    node_inserido.pai.cor = 0
+                    node_inserido.pai.pai.cor = 1
+                    self._rotacionar_esquerda(node_inserido.pai.pai)
+
+            else:
+                tio = node_inserido.pai.pai.direita
+                if tio.cor == 1:
+                    tio.cor = 0
+                    node_inserido.pai.cor = 0
+                    node_inserido.pai.pai.cor = 1
+                    node_inserido = node_inserido.pai.pai
+
+                else:
+                    if node_inserido == node_inserido.pai.direita:
+                        node_inserido = node_inserido.pai
+                        self._rotacionar_esquerda(node_inserido)
+
+                    node_inserido.pai.cor = 0
+                    node_inserido.pai.pai.cor = 1
+                    self._rotacionar_direita(node_inserido.pai.pai)
+
+            if node_inserido == self.raiz:
+                break
+
+        self.raiz.cor = 0
+
+    def pesquisar(self, chave):
+        node = self._pesquisar_recursivo(self.raiz, chave)
+        if node != self.NIL:
+            print(f"Achou a chave {chave}")
+            return node
+        
+        print(f"Não achou a chave {chave}")
+        return None
+
+    def _pesquisar_recursivo(self, node, chave):
+        if node == self.NIL or chave == node.chave:
+            return node
+        
+        if chave < node.chave:
+            return self._pesquisar_recursivo(node.esquerda, chave)
+        
+        return self._pesquisar_recursivo(node.direita, chave)
+
+    def mostrar(self):
+        if self.raiz == self.NIL:
+            print("Árvore Vazia")
+            return
+        
+        self._imprimir_arvore(self.raiz, "", True)
+
+    def _imprimir_arvore(self, node, prefixo="", cauda=True):
+        if node.direita != self.NIL:
+            prefixo_novo = prefixo + ("│   " if cauda else "    ")
+            self._imprimir_arvore(node.direita, prefixo_novo, False)
+        
+        
+        if node.cor == 1:
+            texto = f"P {node.chave}"
+        else:
+            texto = f"V {node.chave}"
+
+        print(prefixo + ("└── " if cauda else "┌── ") + texto)
+        
+        if node.esquerda != self.NIL:
+            prefixo_novo = prefixo + ("    " if cauda else "│   ")
+            self._imprimir_arvore(node.esquerda, prefixo_novo, True)
+
+arvore_rubro_negra = ArvoreRubroNegra()
+valores = [50, 25, 75, 15, 35, 60, 120, 10, 68, 90, 125, 83, 100, 25, 3, 7, 12, 84, 302, 1, 123, 101]
+
+for valor in valores:
+    arvore_rubro_negra.inserir(valor)
+
+chave = 60
+print(f"Pesquisando chave {chave}:")
+arvore_rubro_negra.pesquisar(chave)
+
+arvore_rubro_negra.mostrar()
